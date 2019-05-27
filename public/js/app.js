a9baeef2b8c38ec0ff504049ddc6c1e9b7490903
Ivo Huigsloot <i.huigsloot@ziggo.nl>
2019-05-27 22:12:02 +0200

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherFrom.addEventListener('submit', (e) => {
 
     const location = search.value
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
